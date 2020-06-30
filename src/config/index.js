a27814a438603327729cee0f4b0d42e4a29a3cef
sarath k s <code-freakz@users.noreply.github.com>
2020-06-30 21:58:36 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 const onProduction = process.env.NODE_ENV === 'production';
 
-export const BASE_URL = onProduction ? 'https://pods.com:30006/' : 'http://localhost:4000/';
+export const BASE_URL = onProduction ? 'http://app.phantompod.co:4000' : 'http://localhost:4000/';
 export const AUTH_TOKEN_KEY = 'usertoken';
