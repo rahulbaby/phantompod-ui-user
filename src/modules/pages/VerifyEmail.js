967ec649da84a16bb859466e4fde26140cf622aa
Anoop TD <anooptd@Anoops-MacBook-Pro.local>
2020-07-13 20:36:40 +0530

    verify message

--- src/modules/pages/VerifyEmail.js
+++ src/modules/pages/VerifyEmail.js
@@ -2,18 +2,21 @@ import React, { useEffect, Fragment, useState } from 'react';
 import { LayoutPlain } from 'layouts';
 import { LoaderCircular } from 'components/loaders';
 import { useRedux, useRouter } from 'hooks';
-import { singOut } from 'modules/auth/actions';
+import { emailVerify } from 'modules/auth/actions';
+import { instance } from 'utils';
 
 export default () => {
 	const { dispatch } = useRedux();
 	const { history, query } = useRouter();
 	useEffect(() => {
 		//API call
-		console.log('query', query);
+		instance.post('user/verifyUser',query).then((res) => {
+			console.log('res data:',res);
+		});
 	}, []);
 	return (
 		<LayoutPlain>
-			<LoaderCircular />
+			<LoaderCircular/>
 		</LayoutPlain>
 	);
 };
