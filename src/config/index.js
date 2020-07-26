cfb3c2c68d4af8ae9af3cccf24c012dd93193dc5
Anoop TD <anooptd@Anoops-MacBook-Pro.local>
2020-07-27 00:13:06 +0530

    url changes

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const onProduction = process.env.NODE_ENV === 'production';
 
-export const BASE_URL = onProduction ? 'https://app.phantompod.co/api/' : 'http://localhost:4000/';
+export const BASE_URL = onProduction ? 'https://backend.phantompod.co/' : 'http://localhost:4000/';
 export const AUTH_TOKEN_KEY = 'usertoken';
 export const REACT_APP_STRIPE_PUBLISHABLE_KEY = 'pk_test_F1U8ZiSPK9gAcjttJZTUiwrZ';
