78d4803c0f04c979f01f9f06d3418a229afd287d
code-freakz <marampilly.sarath@gmail.com>
2020-07-27 18:00:18 +0530

    stripe live

--- src/config/index.js
+++ src/config/index.js
@@ -2,4 +2,5 @@ const onProduction = process.env.NODE_ENV === 'production';
 
 export const BASE_URL = onProduction ? 'http://3.17.254.107:4000/' : 'http://localhost:4000/';
 export const AUTH_TOKEN_KEY = 'usertoken';
-export const REACT_APP_STRIPE_PUBLISHABLE_KEY = 'pk_test_F1U8ZiSPK9gAcjttJZTUiwrZ';
+export const REACT_APP_STRIPE_PUBLISHABLE_KEY =
+	'pk_live_51GvxMtDZgeAkgxKXMx1RDDnnchtaxGX0ErwjatFqxa4N2hiBZQuHrAvcsSTOqaWfeCIq7dcKpbnbZCuXeOpAscHr00ppexAkmY';
