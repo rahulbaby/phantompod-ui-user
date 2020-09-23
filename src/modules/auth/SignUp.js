5177e8432cb425ce2728466857f3b31a8b27d69b
Rahulbaby <49438955+Rahulbaby@users.noreply.github.com>
2020-09-23 12:08:39 +0530

    Update SignUp.js

--- src/modules/auth/SignUp.js
+++ src/modules/auth/SignUp.js
@@ -6,15 +6,29 @@ import SignUpForm from './components/SignUpForm';
 
 const GOOGLE_AUTH_URL = `${BASE_URL}auth/google`;
 
+
+function newTab() { 
+	var popup=window.open(GOOGLE_AUTH_URL, 
+	"", "width=500, height=400"); 
+	setTimeout(function(){ 
+		popup.close();
+		window.location.reload();
+	}, 6500);
+	
+}
+
+
+
 export default () => (
 	<LayoutAuth>
 		<div className="login-form">
 			<SignUpForm />
 		</div>
 		<div className="with-google">
-			<button className="btn google-btn" onClick={() => (window.location = GOOGLE_AUTH_URL)}>
+			<button className="btn google-btn" onClick={newTab}>
 				<img src="/img/icons/google-icon.png" alt="" /> Sign In with Google
 			</button>
+			
 			<h6 className="signup-link">
 				Already a member?{` `}
 				<LinkCustom to="/login">Sign In</LinkCustom>
