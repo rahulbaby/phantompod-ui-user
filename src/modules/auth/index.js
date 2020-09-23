294e51e6b72baaf3f4f82b53fad6936512674f28
Rahulbaby <49438955+Rahulbaby@users.noreply.github.com>
2020-09-23 12:09:46 +0530

    Update index.js

--- src/modules/auth/index.js
+++ src/modules/auth/index.js
@@ -6,13 +6,24 @@ import LoginForm from './components/LoginForm';
 
 const GOOGLE_AUTH_URL = `${BASE_URL}auth/google`;
 
+function newTab() { 
+	var popup=window.open(GOOGLE_AUTH_URL, 
+	"", "width=500, height=400"); 
+	setTimeout(function(){ 
+		popup.close();
+		window.location.reload();
+	}, 6500);
+}
+
 export default () => (
 	<LayoutAuth>
 		<div className="login-form">
 			<LoginForm />
-		</div>
+		</div> 
 		<div className="with-google">
-			<button className="btn google-btn" onClick={() => (window.location = GOOGLE_AUTH_URL)}>
+			
+			<button className="btn google-btn" onClick={newTab}>
+			   
 				<img src="/img/icons/google-icon.png" alt="" /> Sign In with Google
 			</button>
 			<h6 className="signup-link">
